de84fff75c2e4fedd617b08005df24d1dfa92d66
Saloni <saloninarayana1@gmail.com>
2025-06-26 10:07:49 +0530

    fix: corrected user registration route

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
       return;
     }
     try {
-      const res = await axios.post( `${import.meta.env.VITE_API_URL}/api/team/register`, formData );
+      const res = await axios.post( `${import.meta.env.VITE_API_URL}/api/auth/register`, formData );
 
       const data = res.data;
 
